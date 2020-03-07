d6ade42b44edd1b2878080289f5f4c26e2f51f9b
Pranjul Sharma <pranjuls@geekyants.com>
2020-03-07 12:55:49 +0530

    (feat/search)-added search bar component in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.scss';
 
-import React, { Component } from 'react'
-import { RESTAURANTS_URL, NO_RESTAURANT_FOUND, SOMETHING_WENT_WRONG, MY_RESTAURANT_KEYS, RESTAURANT_RESPONSE_KEYS } from './shared/constants';
+import React, { Component } from 'react';
+import {
+  RESTAURANTS_URL,
+  NO_RESTAURANT_FOUND,
+  SOMETHING_WENT_WRONG,
+  MY_RESTAURANT_KEYS,
+  RESTAURANT_RESPONSE_KEYS
+} from './shared/constants';
 import RestaurantCard from './components/RestaurantCard';
+import SearchBar from './components/SearchBar';
 
 export class App extends Component {
   constructor(props) {
@@ -10,67 +17,108 @@ export class App extends Component {
     this.state = {
       ld_restaurantsList: true,
       er_restaurantsList: null,
-      restaurantsList: []
-    }
+      restaurantsList: [],
+      allRestaurants: [],
+      searchStr: ''
+    };
   }
 
   getRestaurants = async () => {
-    await this.setState({ ld_restaurantsList: true});
-    fetch(RESTAURANTS_URL).then(res => res.json()).then(restaurants => {
-      if(restaurants && restaurants.length) {
-        let restaurantsList = restaurants.map(restaurant => ({
-          [MY_RESTAURANT_KEYS.BRAND]:
-            restaurant[RESTAURANT_RESPONSE_KEYS.BRAND],
-          [MY_RESTAURANT_KEYS.VARIETY]: restaurant[RESTAURANT_RESPONSE_KEYS.VARIETY],
-          [MY_RESTAURANT_KEYS.STYLE]: restaurant[RESTAURANT_RESPONSE_KEYS.STYLE],
-          [MY_RESTAURANT_KEYS.COUNTRY]: restaurant[RESTAURANT_RESPONSE_KEYS.COUNTRY],
-          [MY_RESTAURANT_KEYS.STARS]: restaurant[RESTAURANT_RESPONSE_KEYS.STARS],
-          [MY_RESTAURANT_KEYS.TOP_TEN]: restaurant[RESTAURANT_RESPONSE_KEYS.TOP_TEN],
-        }));
-        this.setState({
-          ld_restaurantsList: false,
-          restaurantsList
-        });
-      } else {
-        this.setState({
-          ld_restaurantsList: false,
-          er_restaurantsList: NO_RESTAURANT_FOUND
-        });
-      }
-    }).catch(e => {
-      this.setState({
-        ld_restaurantsList: false,
-        er_restaurantsList: SOMETHING_WENT_WRONG
+    await this.setState({ ld_restaurantsList: true });
+    fetch(RESTAURANTS_URL)
+      .then(res => res.json())
+      .then(restaurants => {
+        if (restaurants && restaurants.length) {
+          let restaurantsList = restaurants.map(restaurant => ({
+            [MY_RESTAURANT_KEYS.BRAND]:
+              restaurant[RESTAURANT_RESPONSE_KEYS.BRAND],
+            [MY_RESTAURANT_KEYS.VARIETY]:
+              restaurant[RESTAURANT_RESPONSE_KEYS.VARIETY],
+            [MY_RESTAURANT_KEYS.STYLE]:
+              restaurant[RESTAURANT_RESPONSE_KEYS.STYLE],
+            [MY_RESTAURANT_KEYS.COUNTRY]:
+              restaurant[RESTAURANT_RESPONSE_KEYS.COUNTRY],
+            [MY_RESTAURANT_KEYS.STARS]:
+              restaurant[RESTAURANT_RESPONSE_KEYS.STARS],
+            [MY_RESTAURANT_KEYS.TOP_TEN]:
+              restaurant[RESTAURANT_RESPONSE_KEYS.TOP_TEN]
+          }));
+          this.setState({
+            ld_restaurantsList: false,
+            restaurantsList,
+            allRestaurants: restaurantsList
+          });
+        } else {
+          this.setState({
+            ld_restaurantsList: false,
+            er_restaurantsList: NO_RESTAURANT_FOUND
+          });
+        }
       })
-    });
-  }
+      .catch(e => {
+        this.setState({
+          ld_restaurantsList: false,
+          er_restaurantsList: SOMETHING_WENT_WRONG
+        });
+      });
+  };
 
   componentDidMount() {
     this.getRestaurants();
   }
 
-  handleReload = (e) => {
+  handleReload = e => {
     e.preventDefault();
     this.getRestaurants();
-  }
+  };
 
   renderRestaurant = (restaurant, key) => (
     <React.Fragment key={String(key + 1)}>
       <RestaurantCard restaurant={restaurant} />
     </React.Fragment>
-  )
+  );
+
+  onSearchBoxInput = async e => {
+    await this.setState({
+      searchStr: e.target.value
+    });
+    this.onSearchClicked(e);
+  };
+
+  onSearchClicked = e => {
+    e.preventDefault();
+    const { searchStr, allRestaurants } = this.state;
+    const filteredRestaurants = allRestaurants.filter(restaurant =>
+      restaurant[MY_RESTAURANT_KEYS.BRAND].toLowerCase().includes(searchStr)
+    );
+    this.setState({
+      restaurantsList: filteredRestaurants
+    });
+  };
 
   render() {
-    const { ld_restaurantsList, er_restaurantsList, restaurantsList } = this.state;
+    const {
+      searchStr,
+      ld_restaurantsList,
+      er_restaurantsList,
+      restaurantsList
+    } = this.state;
     return (
       <div className="App">
+        <SearchBar
+          onInput={this.onSearchBoxInput}
+          onSearchClicked={this.onSearchClicked}
+          searchStr={searchStr}
+        />
         <div id="restuarant-list-container">
-          {restaurantsList && restaurantsList.map((restaurant, index) => this.renderRestaurant(restaurant, index))}
+          {restaurantsList &&
+            restaurantsList.map((restaurant, index) =>
+              this.renderRestaurant(restaurant, index)
+            )}
         </div>
-        
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
