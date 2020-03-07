aa182f73f569c83d92c20905ef7c2d43a1cd81e2
Pranjul Sharma <pranjuls@geekyants.com>
2020-03-07 13:49:57 +0530

    (feat/filters)-updated constants and style

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,6 +1,7 @@
 export const RESTAURANTS_URL = 'http://starlord.hackerearth.com/TopRamen';
 export const NO_RESTAURANT_FOUND = 'No Restaurant Found!';
 export const SOMETHING_WENT_WRONG = 'Something went wrong!';
+export const LOADING_RESTAURANTS = 'Loading restaurants...';
 export const TRY_AGAIN = 'Try Again';
 export const RELOAD = 'Reload';
 
@@ -11,7 +12,7 @@ export const RESTAURANT_RESPONSE_KEYS = {
   COUNTRY: 'Country',
   STARS: 'Stars',
   TOP_TEN: 'Top Ten'
-}
+};
 
 export const MY_RESTAURANT_KEYS = {
   BRAND: 'brand',
@@ -20,8 +21,10 @@ export const MY_RESTAURANT_KEYS = {
   COUNTRY: 'country',
   STARS: 'stars',
   TOP_TEN: 'topTen'
-}
+};
 
 export const NAN = 'nan';
 export const NOT_AVAILABLE = 'Not Available';
-export const SEARCH_PLACEHOLDER = 'Search restaurants'
+export const SEARCH_PLACEHOLDER = 'Search restaurants';
+export const KEY_ALL = 'All';
+export const CLEAR_FILTER = 'Clear Filters';
