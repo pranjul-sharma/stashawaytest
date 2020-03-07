c3eee6f5411e8f554a2641072e92d82189b9e52e
Pranjul Sharma <pranjuls@geekyants.com>
2020-03-07 12:07:49 +0530

    (feat/restaurants)-added constants

---
+++ src/shared/constants.js
@@ -0,0 +1,23 @@
+export const RESTAURANTS_URL = 'http://starlord.hackerearth.com/TopRamen';
+export const NO_RESTAURANT_FOUND = 'No Restaurant Found!';
+export const SOMETHING_WENT_WRONG = 'Something went wrong!';
+export const TRY_AGAIN = 'Try Again';
+export const RELOAD = 'Reload';
+
+export const RESTAURANT_RESPONSE_KEYS = {
+  BRAND: 'Brand',
+  VARIETY: 'Variety',
+  STYLE: 'Style',
+  COUNTRY: 'Country',
+  STARS: 'Stars',
+  TOP_TEN: 'Top Ten'
+}
+
+export const MY_RESTAURANT_KEYS = {
+  BRAND: 'brand',
+  VARIETY: 'variety',
+  STYLE: 'style',
+  COUNTRY: 'country',
+  STARS: 'stars',
+  TOP_TEN: 'topTen'
+}
